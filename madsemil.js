7cf3e3fc220a3c79a751d75a93463683d13b78ff
madsemilgodstil <mads93n2@stud.kea.dk>
2024-02-01 12:00:28 +0100

    aa

--- madsemil.js
+++ madsemil.js
@@ -5,5 +5,5 @@
   isEvil: 'false'
   hasCape: 'true'
   age: 31
-  img: 'noget fra unspalsh'
+  img: 'https://images.unsplash.com/photo-1610568781018-995405522539?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 }
